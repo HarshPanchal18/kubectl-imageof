76a57c85a9a1d922afec0cc874d8874bf838bd78
Harsh Panchal <harshhhh1803@gmail.com>
2025-08-26 13:05:11 +0530

    Tried out printing all the pods of given namespace

--- main.go
+++ main.go
@@ -17,17 +17,16 @@ func main() {
 	flag.StringVar(&namespace, "n", "default", "Namespace")
 	flag.Parse()
 
-	args := flag.Args()
-	if(len(args)) < 1 {
-		fmt.Fprintf(os.Stderr, "Usage: kubectl-imageof POD_NAME [flags]\n")
-		os.Exit(1)
-	}
-	podName := args[0]
+	// args := flag.Args()
+	// if(len(args)) < 1 {
+	// 	fmt.Fprintf(os.Stderr, "Usage: kubectl-imageof POD_NAME [flags]\n")
+	// 	os.Exit(1)
+	// }
+	// podName := args[0]
 
 	// Load kubeconfig
 	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	// config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
@@ -37,15 +36,22 @@ func main() {
 		panic(err.Error())
 	}
 
-	pod, err := client.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	// List all pods in the namespace
+    pods, err := client.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
         os.Exit(1)
-	}
+    }
 
-	// Print images
-    fmt.Printf("Image(s) for pod %s:\n", pod.Name)
-    for _, c := range pod.Spec.Containers {
-        fmt.Printf("  %s: %s\n", c.Name, c.Image)
+    if len(pods.Items) == 0 {
+        fmt.Printf("No pods found in namespace %s\n", namespace)
+        return
+    }
+
+    for _, pod := range pods.Items {
+        fmt.Printf("Image(s) for pod %s:\n", pod.Name)
+        for _, container := range pod.Spec.Containers {
+            fmt.Printf("  %s: %s\n", container.Name, container.Image)
+        }
     }
 }
